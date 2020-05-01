682d28c91e8df6c063b73ade6087f527c1d37bb1
madhusudan babar <madhusudanbabar@gmail.com>
2020-05-01 17:02:51 +0530

    styled with angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+import { MatButtonModule  } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,9 +3,9 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button nbButton info *ngIf="!value">{{ value }}</button>
-    <button hero status="success" *ngIf="value =='X'"> {{ value }} </button>
-    <button nbButton hero status="info" *ngIf="value =='O'"> {{ value }} </button>
+    <button mat-stroked-button color="primary" *ngIf="!value">{{ value }}</button>
+    <button mat-stroked-button color="accent" *ngIf="value =='X'"> {{ value }} </button>
+    <button mat-stroked-button color="primary" *ngIf="value =='O'"> {{ value }} </button>
   `,
   styleUrls: ['./square.styles.scss' ] 
 })
